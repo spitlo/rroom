29b3a64ae80c9747ef7ed8be9bbfaed976e9e001
Jakob Bergman <jakob@aino.se>
2024-06-27 14:38:43 +0200

    Start in Rainbow Room

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ initStrudel()
 // |__/ | ___] | \_
 // ------------ ---  --- >
 const gameDisk = () => ({
-  roomId: 'violetroom',
+  roomId: 'rainbowroom',
   cpm: 60,
   isPlaying: false,
   inventory: [],
